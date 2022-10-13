bcd45c55b2694277d3f85626ec22d028d9f31840
Dinolan-Naidoo <1842861@students.wits.ac.za>
2022-10-13 21:06:12 +0200

    Added Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float defDistanceRay = 100;
+
+    public Transform firePoint;
+    public LineRenderer lineRenderer;
+
+    Transform transform;
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        if(Physics2D.Raycast(transform.position,transform.right))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(transform.position, transform.right);
+            Draw2DRay(firePoint.position, _hit.point);
+        }
+        else
+        {
+            Draw2DRay(firePoint.position, firePoint.transform.right * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+    }
+}
